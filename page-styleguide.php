24c51edbc463a001575261ed2131c6c7ee2f2ede
Trevor Allen <ampleheights@gmail.com>
2015-05-20 21:55:24 -0400

    avoid disaster

--- page-styleguide.php
+++ page-styleguide.php
@@ -4,16 +4,16 @@
 <?php endwhile; ?>
 
 <h1>Style Guide 1.0<a href="http://codyhouse.co/?p=459">Download</a></h1>
-		<div class="cd-intro">
+		<div class="qf-intro">
 			<a href="http://codyhouse.co/?p=459" class="btn btn-outline">Article &amp; Download</a>
 		</div>
 
-		<section id="branding" class="cd-branding">
+		<section id="branding" class="qf-branding">
 			<h2>Branding</h2>
 
 			<ul>
-				<li class="cd-box"><img src="assets/branding/logo-ver-1.jpg" alt="Logo version 1"></li>
-				<li class="cd-box"><img src="assets/branding/logo-ver-2.jpg" alt="Logo version 2"></li>
+				<li class="qf-box"><img src="assets/branding/logo-ver-1.jpg" alt="Logo version 1"></li>
+				<li class="qf-box"><img src="assets/branding/logo-ver-2.jpg" alt="Logo version 2"></li>
 			</ul>
 
 			<ul>
@@ -24,51 +24,51 @@
 			</ul>
 		</section>
 
-		<section id="colors" class="cd-colors">
+		<section id="colors" class="qf-colors">
 			<h2>Colors</h2>
 
 			<ul>
-				<li class="cd-box">
-					<div class="cd-color-swatch"></div><b>#5f8ee4</b>
+				<li class="qf-box">
+					<div class="qf-color-swatch"></div><b>#5f8ee4</b>
 				</li>
 
-				<li class="cd-box">
-					<div class="cd-color-swatch"></div><b>#19191a</b>
+				<li class="qf-box">
+					<div class="qf-color-swatch"></div><b>#19191a</b>
 				</li>
 
-				<li class="cd-box">
-					<div class="cd-color-swatch"></div><b>#e4655f</b>
+				<li class="qf-box">
+					<div class="qf-color-swatch"></div><b>#e4655f</b>
 				</li>
 
-				<li class="cd-box">
-					<div class="cd-color-swatch"></div><b>#e6e6e6</b>
+				<li class="qf-box">
+					<div class="qf-color-swatch"></div><b>#e6e6e6</b>
 				</li>
 
-				<li class="cd-box">
-					<div class="cd-color-swatch"></div><b>#ffffff</b>
+				<li class="qf-box">
+					<div class="qf-color-swatch"></div><b>#ffffff</b>
 				</li>
 			</ul>
 		</section>
 
-		<section id="typography" class="cd-typography">
+		<section id="typography" class="qf-typography">
 			<h2>Typography</h2>
 
-			<div class="cd-box">
+			<div class="qf-box">
 				<h1>Heading, <span>300 Open Sans 40px</span></h1>
 				<p>Aa - <span>400 Merriweather 16px</span>. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptate distinctio reprehenderit, autem deleniti ad <a href="#0">voluptatum eaque</a>. Optio ea aperiam nisi distinctio nemo repellat, voluptate fugiat. Quidem neque illum, blanditiis!</p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero mollitia modi ad id quasi illo! Provident corporis unde inventore impedit vel, consequuntur hic eaque officia praesentium! Debitis doloribus fugiat eaque porro eveniet culpa possimus voluptatibus dolores, veritatis quis rem et modi exercitationem, excepturi, ex ratione iste aspernatur sint minus obcaecati illum voluptatum! Eum dicta eius aut qui quo, consectetur vel commodi nam illo facere, quam labore veritatis nihil saepe quisquam quia exercitationem eaque aliquam itaque laudantium quod. Dolorem, explicabo, dignissimos.</p>
 			</div>
 		</section>
 
-		<section id="buttons" class="cd-buttons">
+		<section id="buttons" class="qf-buttons">
 			<h2>Buttons</h2>
 
-			<div class="cd-box">
+			<div class="qf-box">
 				<button class="btn">Normal</button>
 				<button class="btn btn-success">Success</button>
 				<button class="btn btn-alert">Alert</button>
 				<button class="btn btn-outline">Outline</button>
-			</div><div class="cd-box"><p>
+			</div><div class="qf-box"><p>
 				&lt;button class="<span>btn</span>"&gt;Normal&lt;/button&gt;</p><p>
 				&lt;button class="<span>btn btn-success</span>"&gt;Success&lt;/button&gt;</p><p>
 				&lt;button class="<span>btn btn-alert</span>"&gt;Alert&lt;/button&gt;</p><p>
@@ -76,25 +76,25 @@
 
 		</section>
 
-		<section id="icons" class="cd-icons">
+		<section id="icons" class="qf-icons">
 			<h2>Icons</h2>
 
-			<ul class="cd-box">
-				<li class="cd-icon-1"></li>
-				<li class="cd-icon-2"></li>
-				<li class="cd-icon-3"></li>
-				<li class="cd-icon-4"></li>
-				<li class="cd-icon-5"></li>
-				<li class="cd-icon-6"></li>
-				<li class="cd-icon-7"></li>
-				<li class="cd-icon-8"></li>
+			<ul class="qf-box">
+				<li class="qf-icon-1"></li>
+				<li class="qf-icon-2"></li>
+				<li class="qf-icon-3"></li>
+				<li class="qf-icon-4"></li>
+				<li class="qf-icon-5"></li>
+				<li class="qf-icon-6"></li>
+				<li class="qf-icon-7"></li>
+				<li class="qf-icon-8"></li>
 			</ul>
 		</section>
 
-		<section id="form" class="cd-form">
+		<section id="form" class="qf-form">
 			<h2>Form</h2>
 
-			<div class="cd-box">
+			<div class="qf-box">
 				<form>
 					<fieldset>
 						<input type="text">
@@ -103,17 +103,17 @@
 					</fieldset>
 
 					<fieldset>
-						<div class="cd-input-wrapper">
+						<div class="qf-input-wrapper">
 							<input type="radio" name="radioButton" id="radio1">
 	      					<label class="radio-label" for="radio1">Choice 1</label>
       					</div>
 
-						<div class="cd-input-wrapper">
+						<div class="qf-input-wrapper">
 							<input type="checkbox" id="checkbox1">
 	      					<label class="checkbox-label" for="checkbox1">Option 1</label>
       					</div>
 
-						<div class="cd-input-wrapper cd-select">
+						<div class="qf-input-wrapper qf-select">
 	      					<select name="select-this" id="select-this">
 					          <option value="0">Select</option>
 					          <option value="1">Option 1</option>
@@ -125,3 +125,96 @@
 				</form>
 			</div>
 		</section>
+
+<section class="typography">
+  <h1>The Earth <br><i class="normal">The Starting Point</i></h1>
+
+    <a href="http://hypertexthero.com/work/earth-among-the-stars" title="Earth Among The Stars"><img class="pull-left" src="earth.png" alt=""></a>
+
+    <p class="lead">One night when I had tasted bitterness I went out on to the hill. Dark heather checked my feet. Below marched the suburban lamps.</p>
+
+    <p>Windows, their curtains drawn, were shut eyes, inwardly watching the lives of dreams. Beyond the sea’s level darkness a lighthouse pulsed. Overhead, obscurity. I distinguished our own house, our islet in the tumultuous and bitter currents of the world. There, for a decade and a half, we two, so different in quality, had grown in and in to one another, for mutual support and nourishment, in intricate symbiosis. There daily we planned our several undertakings, and recounted the day’s oddities and vexations. There letters piled up to be answered, socks to be darned. There the children were born, those sudden new lives. There, under that roof, our own two lives, recalcitrant sometimes to one another, were all the while thankfully one, one larger, more <a href="http://ebooks.adelaide.edu.au/s/stapledon/olaf/star/chapter1.html#chapter1">conscious life</a> than either alone.</p>
+
+    <h2>All This, Surely, Was Good</h2>
+
+    <p>Yet there was bitterness. And bitterness not only invaded us from the world; it welled up also within our own magic circle. For horror at our futility, at our own unreality, and not only at the world’s delirium, had driven me out on to the hill.</p>
+    <p>We were always hurrying from one little urgent task to another, but the upshot was insubstantial. Had we, perhaps, misconceived our whole existence? Were we, as it were, living from false premises? And in particular, this partnership of ours, this seemingly so well-based fulcrum for activity in the world, was it after all nothing but a little eddy of complacent and ingrown domesticity, ineffectively whirling on the surface of the great flux, having in itself no depth of being, and no significance? Had we perhaps after all deceived ourselves? Behind those rapt windows did we, like so many others, indeed live only a dream? In a sick world even the hale are sick. And we two, spinning our little life mostly by rote, seldom with clear cognizance, seldom with firm intent, were products of a sick world.</p>
+    <p>Yet this life of ours was not all sheer and barren fantasy. Was it not spun from the actual fibres of reality, which we gathered in with all the comings and goings through our door, all our traffic with the suburb and the city and with remoter cities, and with the ends of the earth? And were we not spinning together an authentic expression of our own nature? Did not our life issue daily as more or less firm threads of active living, and mesh itself into the growing web, the intricate, ever-proliferating pattern of mankind?</p>
+
+    <h3>I Considered “Us” With Quiet Interest</h3>
+
+    <p>And a kind of amused awe. How could I describe our relationship even to myself without either disparaging it or insulting it with the tawdry decoration of sentimentality? For this our delicate balance of dependence and independence, this coolly critical, shrewdly ridiculing, but loving mutual contact, was surely a microcosm of true community, was after all in its simple style an actual and living example of that high goal which the world seeks.</p>
+
+    <blockquote>
+      <p>I reflected that not one of the visible features of this celestial and living gem revealed the presence of man. Displayed before me, though invisible, were some of the most congested centers of human population. There below me lay huge industrial regions, blackening the air with smoke. Yet all this thronging life and humanly momentous enterprise had made no mark whatever on the features of the planet. From this high look-out the Earth would have appeared no different before the dawn of man. No visiting angel, or explorer from another planet, could have guessed that this bland orb teemed with vermin, with world-mastering, self-torturing, incipiently angelic beasts. <small>— Olaf Stapledon, <cite title="Star Maker">Star Maker</cite></small></p>
+    </blockquote>
+
+    <p>The whole world? The whole universe? Overhead, obscurity unveiled a star. One tremulous arrow of light, projected how many thousands of years ago, now stung my nerves with vision, and my heart with fear. For in such a universe as this what significance could there be in our fortuitous, our frail, our evanescent community?</p>
+
+    <h4>But Now Irrationally I Was Seized</h4>
+
+    <p>With a strange worship, not, surely of the star, that mere furnace which mere distance falsely sanctified, but of something other, which the dire contrast of the star and us signified to the heart. Yet what, what could thus be signified? Intellect, peering beyond the star, discovered no Star Maker, but only darkness; no Love, no Power even, but only Nothing.</p>
+
+    <h5>And Yet the Heart Praised</h5>
+
+    <p>Impatiently I shook off this folly, and reverted from the inscrutable to the familiar and the concrete. Thrusting aside worship, and fear also and bitterness, I determined to examine more coldly this remarkable “us,” this surprisingly impressive datum, which to ourselves remained basic to the universe, though in relation to the stars it appeared so slight a thing.</p>
+
+    <h6>Considered Even Without Reference to Our Belittling Cosmical Background</h6>
+
+    <p>We were after all insignificant, perhaps ridiculous. We were such a commonplace occurrence, so trite, so respectable. We were just a married couple, making shift to live together without undue strain. Marriage in our time was suspect. And ours, with its trivial romantic origin, was doubly suspect.</p>
+
+    <ul>
+      <li>We had first met when she was a child</li>
+      <li>Our eyes encountered</li>
+      <li>She looked at me for a moment with quiet attention</li>
+      <li>Even, I had romantically imagined, with obscure, deep-lying recognition
+        <ul>
+          <li>I, at any rate, recognized in that look</li>
+          <li>So I persuaded myself in my fever of adolescence</li>
+        </ul>
+      </li>
+      <li>My destiny</li>
+    </ul>
+
+    <p><strong>Yes!</strong> <em>How predestinate had seemed our union!</em> Yet now, in retrospect, how accidental. True, <del>of course</del>, that as a long-married couple we <kbd>fitted</kbd> rather neatly, like <small>two close trees whose trunks</small> have grown upwards together as a single shaft, mutually distorting, but mutually supporting. <abbr title="abbreviation title">ABC</abbr></p>
+
+    <ol>
+      <li>The Diversity of Worlds</li>
+      <li>Strange Mankinds</li>
+      <li>Nautiloids</li>
+      <li>The Earth
+        <ul>
+          <li>The Starting Point</li>
+          <li>Earth Among the Stars</li>
+        </ul>
+      </li>
+      <li>More Worlds</li>
+    </ol>
+
+    <p>Here's some code: <code>e = mc<sup>2</sup></code></p>
+
+    <pre>
+
+      <code>And
+        Some
+        Poetry
+      </code>
+    </pre>
+
+    <dl>
+      <dt>Coldly</dt>
+      <dd>I now assessed her as merely a useful, but often infuriating adjunct to my personal life.</dd>
+      <dt>We</dt>
+      <dd>Were on the whole sensible companions.</dd>
+      <dd>We left one another a certain freedom, and so we were able to endure our proximity.</dd>
+      <dt>Such</dt>
+      <dd>Was our relationship.</dd>
+    </dl>
+
+    <hr>
+
+    <p class="quiet">
+      <small>Text excerpt from <a href="http://ebooks.adelaide.edu.au/s/stapledon/olaf/star/"><i>Star Maker</i> by Olaf Stapledon</a></small>
+    </p>
+
+</section>
